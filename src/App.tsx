96542dd5ab87256ceb749edbbd2725bb05e679b3
lucas-ssv <lucas_99silva@hotmail.com>
2021-09-21 15:16:46 -0300

    feat: add new link to the page signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { FormEvent, useState } from "react";
+import { FiLogIn } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import styles from "./styles/home.module.scss";
+import logo from "./assets/logo.svg";
+import { Banner } from "./components/Banner";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
+import "./styles/common.scss";
+
 export function App() {
+  const [userId, setUserId] = useState("");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    console.log(userId);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <section className="container">
+      <main className={styles.gridWrapper}>
+        <div className={styles.logon}>
+          <img src={logo} alt="bethehero" />
+          <p>Faça seu logon</p>
+          <form onSubmit={handleSubmit}>
+            <Input
+              type="text"
+              name="userId"
+              value={userId}
+              onChange={(e) => setUserId(e.target.value)}
+              placeholder="Sua ID"
+            />
+            <Button type="submit">Entrar</Button>
+          </form>
+
+          <Link to="/signup">
+            <FiLogIn size={25} color="#e02041" />
+            Não tenho cadastro
+          </Link>
+        </div>
+
+        <Banner />
+      </main>
+    </section>
   );
 }
