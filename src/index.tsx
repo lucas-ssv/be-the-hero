b60fadb8477edecf1c4f078b4392d5750e58c4ff
lucas-ssv <lucas_99silva@hotmail.com>
2021-09-25 21:36:33 -0300

    feat: add makeServer mirage

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import { makeServer } from "./services/mirage";
 import "./styles/global.scss";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer({ environment: "development" });
+}
 
 ReactDOM.render(
   <React.StrictMode>
