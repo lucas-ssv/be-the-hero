1ff79bb34911f819b1ec00abddf0700e949b8af5
lucas-ssv <lucas_99silva@hotmail.com>
2021-09-25 21:36:04 -0300

    feat: add auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import { Routes } from "./routes";
 
 export function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </BrowserRouter>
   );
 }
