baee139ac9c1603baff8564640cc648a7e5bda9d
lucas-ssv <lucas_99silva@hotmail.com>
2021-09-27 16:17:34 -0300

    feat: add component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { FiPower } from "react-icons/fi";
+import logo from "../assets/logo.svg";
+import "../styles/header.scss";
+import "../styles/common.scss";
+
+export function Header() {
+  return (
+    <header>
+      <div className="info">
+        <img src={logo} alt="bethehero" />
+        <span>Bem vindo, Lucas</span>
+      </div>
+      <div>
+        <button className="newCase">Cadastrar novo caso</button>
+        <button className="power">
+          <FiPower size={24} color="#e02041" />
+        </button>
+      </div>
+    </header>
+  );
+}
