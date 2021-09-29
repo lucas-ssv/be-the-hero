432e82d8c0cf551581a6066cec6422484bfb8c57
lucas-ssv <lucas_99silva@hotmail.com>
2021-09-29 16:46:26 -0300

    feat: add context provider to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Routes } from "./routes";
+import "react-toastify/dist/ReactToastify.css";
+import { CaseProvider } from "./contexts/CaseContext";
 
 export function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes />
+        <CaseProvider>
+          <Routes />
+          <ToastContainer autoClose={3000} theme="colored" />
+        </CaseProvider>
       </AuthProvider>
     </BrowserRouter>
   );
