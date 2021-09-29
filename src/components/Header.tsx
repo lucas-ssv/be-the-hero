d830b96f718862c62a900ede49c8b1e56745adb8
lucas-ssv <lucas_99silva@hotmail.com>
2021-09-29 16:46:59 -0300

    feat: add signOut by context

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,25 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
 import { FiPower } from "react-icons/fi";
 import logo from "../assets/logo.svg";
 import "../styles/header.scss";
 import "../styles/common.scss";
+import { AuthContext } from "../contexts/AuthContext";
 
 export function Header() {
+  const { organization, signOut } = useContext(AuthContext);
+
   return (
     <header>
       <div className="info">
         <img src={logo} alt="bethehero" />
-        <span>Bem vindo, Lucas</span>
+        <span>Bem vindo, {organization.name}</span>
       </div>
       <div>
         <Link to="/create" className="newCase">
           Cadastrar novo caso
         </Link>
-        <button className="power">
+        <button className="power" onClick={signOut}>
           <FiPower size={24} color="#e02041" />
         </button>
       </div>
